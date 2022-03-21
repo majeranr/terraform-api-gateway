3d35f4155cd9c104aeedaa6d028be256c2f43fa0
majeranr <98885715+majeranr@users.noreply.github.com>
2022-03-21 12:23:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@ variable "root_region" {
   type    = string
   default = "us-east-1"
 }
-
-variable "dynamodb_terraform_name" {
-  type    = string
-  default = "aws-terraform-remote-state-locks"
-}
-
