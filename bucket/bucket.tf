ea56ee08aedc6e639daed1fa8764c9eb7d2780de
majeranr <98885715+majeranr@users.noreply.github.com>
2022-03-21 12:28:26 +0100

    Update bucket.tf

--- bucket/bucket.tf
+++ bucket/bucket.tf
@@ -14,21 +14,21 @@ provider "aws" {
   region  = var.bucket_region
 }
 
-resource "aws_s3_bucket" "aws-apitest-lambda-rm" {
+resource "aws_s3_bucket" "aws-apitest-lambda" {
   bucket        = var.bucket_name
   acl           = var.privacy
   force_destroy = "true"
 }
 
 resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.aws-apitest-lambda-rm.id
+  bucket = aws_s3_bucket.aws-apitest-lambda.id
   key    = "index.zip"
   acl    = var.privacy
   source = "${path.root}/index.zip"
 }
 
 output "bucket_id" {
-  value = aws_s3_bucket.aws-apitest-lambda-rm.id
+  value = aws_s3_bucket.aws-apitest-lambda.id
 }
 
 output "bucket_object_id" {
